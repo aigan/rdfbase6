d2462fa5e2ee14c3b68e2e68e9c9ede396b0023c
jonas <jonas>
2007-08-21 20:26:45 +0000

    Gives TT context to Rit::Base::Widge::wub

---
+++ lib/Rit/Base/Widget/Plugins.pm
@@ -0,0 +1,50 @@
+package Rit::Base::Widget::Plugins;
+
+use strict;
+use warnings;
+
+use Template::Constants;
+
+use Para::Frame::Reload;
+
+use Rit::Base::Widget;
+use Rit::Base::Utils qw( parse_propargs );
+
+sub new
+{
+    return bless {}, $_[0];
+}
+
+sub fetch
+{
+    my ($self, $name, $args, $context) = @_;
+
+    warn "Looking for plugin $name\n";
+
+    $self->{ FACTORY } ||=
+    {
+     wub => sub
+     {
+	 my( $context ) = shift;
+	 return sub
+	 {
+	     my( $pred, $args_in ) = @_;
+	     my( $args ) = parse_propargs($args_in);
+	     $args->{'context'} = $context;
+	     return Rit::Base::Widget::wub($pred, $args);
+	 }
+     },
+    };
+
+    my $factory = $self->{ FACTORY }{$name};
+
+    unless( $factory )
+    {
+	return (undef, Template::Constants::STATUS_DECLINED);
+    }
+
+    return  &$factory($context, @$args);
+}
+
+
+1;
