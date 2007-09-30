190bca2c38e6eb0e0d03ea666b407eba858f5d47
jonas <jonas>
2007-09-30 22:29:04 +0000

    For handling pred-specific list methods

---
+++ lib/Rit/Base/Pred/List.pm
@@ -0,0 +1,59 @@
+#  $Id$  -*-cperl-*-
+package Rit::Base::Pred::List;
+#=====================================================================
+#
+# DESCRIPTION
+#   Ritbase Resource Pred List class
+#
+# AUTHOR
+#   Jonas Liljegren   <jonas@paranormal.se>
+#
+# COPYRIGHT
+#   Copyright (C) 2005-2007 Avisita AB.  All Rights Reserved.
+#
+#=====================================================================
+
+=head1 NAME
+
+Rit::Base::Pred::List
+
+=cut
+
+use Carp qw(carp croak cluck confess);
+use strict;
+
+BEGIN
+{
+    our $VERSION  = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
+    print "Loading ".__PACKAGE__." $VERSION\n";
+}
+
+use Para::Frame::Reload;
+use Para::Frame::Utils qw( throw debug datadump  );
+
+use Rit::Base::Utils qw( is_undef valclean query_desig parse_propargs );
+
+### Inherit
+#
+use base qw( Rit::Base::List );
+
+
+=head1 DESCRIPTION
+
+Inherits from L<Rit::Base::List>
+
+=cut
+
+#######################################################################
+
+1;
+
+=head1 SEE ALSO
+
+L<Rit::Base>,
+L<Rit::Base::Resource>,
+L<Rit::Base::Arc>,
+L<Rit::Base::Pred>,
+L<Rit::Base::Search>
+
+=cut
