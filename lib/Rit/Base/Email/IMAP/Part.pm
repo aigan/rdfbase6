dba818f0a1e899dee2749a1fb801bcd348a69edd
jonas <jonas>
2008-02-11 06:57:17 +0000

    more debug

--- lib/Rit/Base/Email/IMAP/Part.pm
+++ lib/Rit/Base/Email/IMAP/Part.pm
@@ -22,13 +22,6 @@ use strict;
 use utf8;
 use Carp qw( croak confess cluck );
 use Scalar::Util qw(weaken);
-#use URI;
-#use MIME::Words qw( decode_mimewords );
-#use IMAP::BodyStructure;
-#use MIME::QuotedPrint qw(decode_qp);
-#use MIME::Base64 qw( decode_base64 );
-#use MIME::Types;
-#use CGI;
 
 BEGIN
 {
@@ -37,23 +30,8 @@ BEGIN
 }
 
 use Para::Frame::Reload;
-#use Para::Frame::Utils qw( throw debug create_dir chmod_file idn_encode idn_decode datadump catch fqdn );
-use Para::Frame::Utils qw( throw debug );
-#use Para::Frame::L10N qw( loc );
+use Para::Frame::Utils qw( throw debug datadump );
 use Para::Frame::List;
-
-use Rit::Base;
-use Rit::Base::Utils qw( parse_propargs alfanum_to_id is_undef );
-use Rit::Base::Constants qw( $C_email );
-use Rit::Base::Literal::String;
-use Rit::Base::Literal::Time qw( now ); #);
-use Rit::Base::Literal::Email::Address;
-use Rit::Base::Literal::Email::Subject;
-use Rit::Base::Email::Head;
-use Rit::Base::Email::Classifier::Bounce;
-use Rit::Base::Email::Classifier::Vacation;
-
-use constant EA => 'Rit::Base::Literal::Email::Address';
 
 use base qw( Rit::Base::Email::Part );
 
@@ -99,6 +77,15 @@ sub new_by_path
     }
 
     my $struct = $part->top->struct->part_at($path);
+
+    unless( $struct )
+    {
+	debug "Failed to get struct at $path";
+	debug "Top: ".$part->top;
+	debug "Top struct: ". $part->top->struct;
+	debug $part->top->desig;
+	confess datadump($part,1);
+    }
 
     my $sub = bless
     {
