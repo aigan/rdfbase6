a5bbed46a8bfe739b6fbad64909e9b0be06e38b2
jonas <jonas>
2008-03-22 05:32:45 +0000

    bugfix

--- lib/Rit/Base/Literal/String.pm
+++ lib/Rit/Base/Literal/String.pm
@@ -274,13 +274,13 @@ sub parse
 	#$val_mod =~ s/\x{fffd}//g;
 
 	# Replace Space separator chars
-	$decoded =~ s/\p{Zs}/ /g;
+	$val_mod =~ s/\p{Zs}/ /g;
 
 	# Replace Line separator chars
-	$decoded =~ s/\p{Zl}/\n/g;
+	$val_mod =~ s/\p{Zl}/\n/g;
 
 	# Replace Paragraph separator chars
-	$decoded =~ s/\p{Zp}/\n\n/g;
+	$val_mod =~ s/\p{Zp}/\n\n/g;
 
 
 	$val_mod =~ s/[ \t]*\r?\n/\n/g; # CR and whitespace at end of line
