e5fd701c3536ba20c91e4f3c2e49f071e5961804
jonas <jonas>
2008-05-09 15:10:17 +0000

    Removed unused module loading

--- lib/Rit/Base/Email/Head.pm
+++ lib/Rit/Base/Email/Head.pm
@@ -26,8 +26,6 @@ use MIME::Words qw( decode_mimewords );
 use IMAP::BodyStructure;
 use MIME::QuotedPrint qw(decode_qp);
 use MIME::Base64 qw( decode_base64 );
-use MIME::Types;
-use CGI;
 
 BEGIN
 {
