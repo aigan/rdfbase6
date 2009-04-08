0586abf0d7fd5465d89364d461be701868a30abe
Jonas Liljegren <jonas@avisita.com>
2009-04-08 14:53:00 +0200

    Bugfix for keeping created dated (cherry picked from commit 1f89e8f39114c9b9a1bf8907dc3065c320c69e06)

--- lib/Rit/Base/Resource.pm
+++ lib/Rit/Base/Resource.pm
@@ -6827,6 +6827,8 @@ sub save
 
 #    debug "Saving node $nid with label ".$node->label;
 
+    # The field_obj variants are initiated on demand
+
     my $dbix = $Rit::dbix;
 
     $node->initiate_node;
@@ -6839,7 +6841,7 @@ sub save
 
     $node->{'read_access'}    ||= $public->id;
     $node->{'write_access'}   ||= $sysadmin_group->id;
-    $node->{'created_obj'}    ||= $now;
+    $node->{'created_obj'}    ||= $node->created || $now;
     delete $node->{'created'};
 
     if( $node->{'created_by_obj'} )
@@ -6848,7 +6850,7 @@ sub save
     }
     $node->{'created_by'}     ||= $uid;
 
-    $node->{'updated_obj'}    ||= $now;
+    $node->{'updated_obj'}    ||= $node->updated || $now;
     delete $node->{'updated'};
 
     if( $node->{'updated_by_obj'} )
@@ -6862,6 +6864,8 @@ sub save
 	$node->{'owned_by'}     = $node->{'owned_by_obj'}->id;
     }
     $node->{'owned_by'}       ||= $node->{'created_by'};
+
+    debug "  saving created ".$node->{'created'};
 
 
     my @values =
