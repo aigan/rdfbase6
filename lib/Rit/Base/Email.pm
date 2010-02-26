3edd36299055b274a5060c27d9a87e3bd6fcac77
Jonas Liljegren <jonas@liljegren.org>
2010-02-26 03:05:07 +0100

    Gives ot_obj to email renderer

--- lib/Rit/Base/Email.pm
+++ lib/Rit/Base/Email.pm
@@ -798,7 +798,10 @@ sub send
 	}
 
 	eval {
-	    $es->send_by_proxy({%$args, to => $to });
+	    $es->send_by_proxy({%$args,
+				to => $to,
+				to_obj => $to_obj,
+			       });
 	};
 	$req->may_yield;
 	( $to_obj, $to_obj_err ) = $to_obj_list->get_next;
