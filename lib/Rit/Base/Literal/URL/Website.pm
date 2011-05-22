b0ce2cce32204be2dd5efcb1c05de43b8c5face0
Jonas Liljegren <jonas@liljegren.org>
2011-05-23 01:25:00 +0200

    Match ip

--- lib/Rit/Base/Literal/URL/Website.pm
+++ lib/Rit/Base/Literal/URL/Website.pm
@@ -94,7 +94,7 @@ sub parse
 	unless( $scheme =~ /^https?$/ )
 	{
 	    my $str = $url->as_string;
-	    if( $str =~ s/^([a-z0-9][a-z0-9\-\.]*\.[a-z]{1,6}):(\d+)//i )
+	    if( $str =~ s/^([a-z0-9][a-z0-9\-\.]*\.[a-z0-9]{1,6}):(\d+)//i )
 	    {
 		my $host = $1;
 		my $port = $2;
@@ -119,7 +119,7 @@ sub parse
     {
 	my $path = $url->path || '';
 	debug 3, "Initial path is $path";
-	if( $path =~ s/^([a-z0-9][a-z0-9\-\.]*\.[a-z]{1,6}\b)//i )
+	if( $path =~ s/^([a-z0-9][a-z0-9\-\.]*\.[a-z0-9]{1,6}\b)//i )
 	{
 	    my $host = $1;
 	    $url->host($host);
@@ -144,7 +144,7 @@ sub parse
     if( my $host = $url->host )
     {
 	debug 3, "Host is now $host";
-	unless( $host =~ /^[a-z0-9][a-z0-9\-\.]*\.[a-z]{1,6}$/ )
+	unless( $host =~ /^[a-z0-9][a-z0-9\-\.]*\.[a-z0-9]{1,6}$/ )
 	{
 	    throw 'validation', loc "Malformed hostname in website URL $url";
 	}
