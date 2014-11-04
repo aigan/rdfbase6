ecfd913c11512b5fea1f61eb4d74db9433eeaa16
Jonas Liljegren <jonas@avisita.com>
2014-11-04 15:17:08 +0100

    Indentation

--- lib/RDF/Base/Email/Address.pm
+++ lib/RDF/Base/Email/Address.pm
@@ -74,7 +74,7 @@ Will B<not> throw an exception if email address is faulty
 
 =cut
 
-sub new
+  sub new
 {
     my( $class, $in_value, $args ) = @_;
 
@@ -83,12 +83,12 @@ sub new
     my $code_in = lc $a->address;
 
     my $an_args = solid_propargs({
-				  default_create =>
-				  {
-				   ea_original => $a,
-				   name => $a->name,
-				  }
-				 });
+                                  default_create =>
+                                  {
+                                   ea_original => $a,
+                                   name => $a->name,
+                                  }
+                                 });
 
     my $an = RDF::Base::Resource->set_one({
                                            code=>$code_in,
@@ -127,9 +127,9 @@ sub exist
     my $an_args = solid_propargs();
 
     return RDF::Base::Resource->find({
-                                     code=>$code_in,
-                                     is=>$C_email_address_holder,
-                                    }, $an_args)->get_first_nos;
+                                      code=>$code_in,
+                                      is=>$C_email_address_holder,
+                                     }, $an_args)->get_first_nos;
 }
 
 
@@ -147,14 +147,14 @@ sub wuirc
     my $out = "";
 
     my $predname;
-    if( ref $pred )
+    if ( ref $pred )
     {
-	$predname = $pred->label;
+        $predname = $pred->label;
     }
     else
     {
-	$predname = $pred;
-	$pred = RDF::Base::Pred->get_by_label($predname);
+        $predname = $pred;
+        $pred = RDF::Base::Pred->get_by_label($predname);
     }
 
 #    debug "  $predname for ".$subj->sysdesig;
@@ -163,9 +163,9 @@ sub wuirc
     my $size = $args->{'size'} ||"";
 
     $args->{'id'} ||= build_field_key({
-				       pred => $predname,
-				       subj => $subj,
-				      });
+                                       pred => $predname,
+                                       subj => $subj,
+                                      });
 
     my $disabled = $args->{'disabled'}||'';
     my $arcversions =  $subj->arcversions($predname);
@@ -174,7 +174,7 @@ sub wuirc
 
     my $multi = $args->{'multi'} // not $pred->range_card_max_1;
 
-    my $onchange = ""; #"RDF.Base.pageparts['$divid'].node_update()";
+    my $onchange = "";  #"RDF.Base.pageparts['$divid'].node_update()";
 
     foreach my $arc ( RDF::Base::List->new(\@arcs)->sorted([{on=>'obj.weight', dir=>'desc'}])->as_array )
     {
@@ -182,9 +182,9 @@ sub wuirc
 
         # Maby show weight
 
-        if( $Para::Frame::U->has_root_access and
-            ( (@{$arcversions->{$arc_id}} > 1) or
-              $arcversions->{$arc_id}[0]->submitted ) )
+        if ( $Para::Frame::U->has_root_access and
+             ( (@{$arcversions->{$arc_id}} > 1) or
+               $arcversions->{$arc_id}[0]->submitted ) )
         {
             # TODO: Handle new/submitted/removed arcs
             die "Implement multiple versions";
@@ -202,43 +202,44 @@ sub wuirc
          arc => $arc->id,
         };
 
-        if( $disabled )
+        if ( $disabled )
         {
             $out .= $arc->value->as_html({method=>'address'});
         }
         else
         {
             my $val = $arc->value;
-            $out .= input($field, $val->code->plain, $fargs);
+            $out .= input($field, $val->code->plain, {tag_attr=>$fargs});
             $out .= ' '.$val->deliverability_as_html($args);
         }
         $out .= "&nbsp;" . $arc->edit_link_html;
         $out .= "<br>";
     }
 
-    if( ($multi or not @arcs) and not $disabled )
+    if ( ($multi or not @arcs) and not $disabled )
     {
         my $props =
-	{
-	 pred => $predname,
-	 subj => $subj,
-	};
+        {
+         pred => $predname,
+         subj => $subj,
+        };
 
         my $default = '';
-        if( my $def = $args->{default_value} )
+        if ( my $def = $args->{default_value} )
         {
             $default = $def->address;
         }
 
         $out .= input( build_field_key($props),
                        $default,
-                       {
-                        class => $args->{'class'},
-                        size => $size,
-                        maxlength => $args->{'maxlength'},
-                        id => $args->{'id'},
-                        onchange => $onchange,
-                       });
+                       { tag_attr =>
+                         {
+                          class => $args->{'class'},
+                          size => $size,
+                          maxlength => $args->{'maxlength'},
+                          id => $args->{'id'},
+                          onchange => $onchange,
+                         }});
     }
 
 
@@ -329,7 +330,7 @@ sub error_message
 
 sub as_string
 {
-     return $_[0]->first_prop('code')->plain;
+    return $_[0]->first_prop('code')->plain;
 }
 
 ##############################################################################
@@ -385,13 +386,13 @@ sub format_human
 
     my $name = $ea->first_prop('name');
 
-    if( $name )
+    if ( $name )
     {
-	return sprintf "%s <%s>", $name, $ea->first_prop('code');
+        return sprintf "%s <%s>", $name, $ea->first_prop('code');
     }
     else
     {
-	return $ea->first_prop('code');
+        return $ea->first_prop('code');
     }
 }
 
@@ -494,27 +495,27 @@ sub deliverability_as_html
 
     my $color;
 
-    if( $ea->broken )
+    if ( $ea->broken )
     {
         $color = 'red';
     }
-    elsif( $ea->has_email_deliverability('ed_agent_away') )
+    elsif ( $ea->has_email_deliverability('ed_agent_away') )
     {
         $color = '#8B0';
     }
-    elsif( $ea->has_email_deliverability('ed_deliverable') )
+    elsif ( $ea->has_email_deliverability('ed_deliverable') )
     {
         $color = '#5F5';
     }
-    elsif( $ea->has_email_deliverability('ed_delayed') )
+    elsif ( $ea->has_email_deliverability('ed_delayed') )
     {
         $color = '#FA0';
     }
-    elsif( $ea->has_email_deliverability('ed_address_changed') )
+    elsif ( $ea->has_email_deliverability('ed_address_changed') )
     {
         $color = '#F0F';
     }
-    elsif( $ea->has_email_deliverability('ed_unclassified') )
+    elsif ( $ea->has_email_deliverability('ed_unclassified') )
     {
         $color = 'blue';
     }
@@ -545,7 +546,7 @@ sub update_deliverability
     my $patience = DateTime::Duration->new(days => 14);
     my $big_patience = DateTime::Duration->new(months => 2);
 
-    if( $c->is_dsn )
+    if ( $c->is_dsn )
     {
         debug "DELIVERY STATUS NOTIFICATION";
 
@@ -561,10 +562,10 @@ sub update_deliverability
     debug "REASON: $std_reason";
 
 
-    if( $c->is_address_changed )
+    if ( $c->is_address_changed )
     {
         my $new = $c->{contact}{email_address}{changed_to};
-        if( $new )
+        if ( $new )
         {
             $ea->move_agent_to($new, $args);
         }
@@ -573,12 +574,12 @@ sub update_deliverability
             debug "New email not found in ".$email->id;
         }
     }
-    elsif( $c->is_quit_work )
+    elsif ( $c->is_quit_work )
     {
-	$ea->update({'has_email_deliverability'=>
-		     C->get('ed_mailbox_unavailible')}, $args);
+        $ea->update({'has_email_deliverability'=>
+                     C->get('ed_mailbox_unavailible')}, $args);
     }
-    elsif( $c->is_bounce )
+    elsif ( $c->is_bounce )
     {
         given( $std_reason )
         {
@@ -615,114 +616,114 @@ sub update_deliverability
             }
         }
     }
-    elsif( $c->is_vacation and $c->dsn_date )
+    elsif ( $c->is_vacation and $c->dsn_date )
     {
-	# 1. Short term
-	# 2. Long term
-	# 3. Reporting time in the past
-	# 4. Unknown time
+        # 1. Short term
+        # 2. Long term
+        # 3. Reporting time in the past
+        # 4. Unknown time
 
-	my $agent_away = C->get('ed_agent_away');
+        my $agent_away = C->get('ed_agent_away');
 
-	my $eda = $ea->arc('has_email_deliverability',$agent_away);
-	my $ed_date = $eda->dsn_date;
-	my $ed = $eda->obj;
+        my $eda = $ea->arc('has_email_deliverability',$agent_away);
+        my $ed_date = $eda->dsn_date;
+        my $ed = $eda->obj;
 
-	my $avail = $c->dsn_date_availible;
+        my $avail = $c->dsn_date_availible;
 #	my $today = now();
-	if( $avail )
-	{
-	    if( $c->dsn_date > $avail + $big_patience )
-	    {
-		# Should have come back a long time ago
-		#
+        if ( $avail )
+        {
+            if ( $c->dsn_date > $avail + $big_patience )
+            {
+                # Should have come back a long time ago
+                #
 #		debug "AV1";
-		$ea->update({'has_email_deliverability' =>
+                $ea->update({'has_email_deliverability' =>
                              C->get('ed_non_deliverable')}, $args);
-	    }
-	    elsif( $c->dsn_date + $patience > $avail )
-	    {
-		# Back within a week
-		#
+            }
+            elsif ( $c->dsn_date + $patience > $avail )
+            {
+                # Back within a week
+                #
 #		debug "AV2";
-		$ea->update({'has_email_deliverability' =>
-			     C->get('ed_deliverable')}, $args);
-	    }
-	    #
-	    # Coming back at a specified future time
-	    #
-	    elsif( $eda and $ed->label('ed_agent_away') )
-	    {
-		# Maby update the time
-		if( $eda->dsn_date_availible < $avail )
-		{
+                $ea->update({'has_email_deliverability' =>
+                             C->get('ed_deliverable')}, $args);
+            }
+            #
+            # Coming back at a specified future time
+            #
+            elsif ( $eda and $ed->label('ed_agent_away') )
+            {
+                # Maby update the time
+                if ( $eda->dsn_date_availible < $avail )
+                {
 #                    debug "AV3";
                     $eda->update({dsn_date_availible => $avail}, $args);
-		}
-	    }
-	    else # Store the time
-	    {
-#		debug "AV4";
-		$eda->remove($args) if $eda;
-		$eda = $ea->add_arc({has_email_deliverability =>
-				     $agent_away}, $args);
-		$eda->add({dsn_date_availible => $avail}, $args);
-	    }
-
-	    # Update the latest time we got a specified return time
-	    #
-#	    debug "AV5";
-	    $eda ||= $ea->arc('has_email_deliverability',$agent_away);
-	    if( $eda->dsn_date )
-	    {
-		if( $eda->dsn_date < $c->dsn_date )
-		{
-		    $eda->arc('dsn_date')->remove;
-		    $eda->add({dsn_date => $c->dsn_date}, $args);
-		}
-	    }
-	    else
-	    {
-		$eda->add({dsn_date => $c->dsn_date}, $args);
-	    }
-	}
-	#
-	# Unspecified return time
-	#
-	elsif( $ed and $ed_date and $ed->label('ed_agent_away') )
-        {
-	    my $long_time = DateTime::Duration->new(months => 12);
-	    if( $c->dsn_date > $ed_date + $long_time )
+                }
+            }
+            else                # Store the time
             {
-		# Probably never coming back...
+#		debug "AV4";
+                $eda->remove($args) if $eda;
+                $eda = $ea->add_arc({has_email_deliverability =>
+                                     $agent_away}, $args);
+                $eda->add({dsn_date_availible => $avail}, $args);
+            }
+
+            # Update the latest time we got a specified return time
+            #
+#	    debug "AV5";
+            $eda ||= $ea->arc('has_email_deliverability',$agent_away);
+            if ( $eda->dsn_date )
+            {
+                if ( $eda->dsn_date < $c->dsn_date )
+                {
+                    $eda->arc('dsn_date')->remove;
+                    $eda->add({dsn_date => $c->dsn_date}, $args);
+                }
+            }
+            else
+            {
+                $eda->add({dsn_date => $c->dsn_date}, $args);
+            }
+        }
+        #
+        # Unspecified return time
+        #
+        elsif ( $ed and $ed_date and $ed->label('ed_agent_away') )
+        {
+            my $long_time = DateTime::Duration->new(months => 12);
+            if ( $c->dsn_date > $ed_date + $long_time )
+            {
+                # Probably never coming back...
 #		debug "AV6";
-		$ea->update({'has_email_deliverability' =>
+                $ea->update({'has_email_deliverability' =>
                              C->get('ed_non_deliverable')}, $args);
-	    }
-	}
-	else # Remeber when we started seeing the vacation
-	{
+            }
+        }
+        else             # Remeber when we started seeing the vacation
+        {
 #		debug "AV7";
             $eda->remove($args) if $eda;
             $eda = $ea->add_arc({has_email_deliverability =>
                                  $agent_away}, $args);
             $eda->add({dsn_date => $c->dsn_date}, $args);
-	}
+        }
     }
-    elsif( $c->is_transient and $c->dsn_date ) # Need the date
+    elsif ( $c->is_transient and $c->dsn_date ) # Need the date
     {
-	my $delayed = C->get('ed_delayed');
-	my $eda = $ea->arc('has_email_deliverability');
-	my $ed_date = $eda->dsn_date;
-	my $ed = $eda->obj;
+        my $delayed = C->get('ed_delayed');
+        my $eda = $ea->arc('has_email_deliverability');
+        my $ed_date = $eda->dsn_date;
+        my $ed = $eda->obj;
 
-        if( $ed and $ed_date and $ed->label('ed_delayed') )
+        if ( $ed and $ed_date and $ed->label('ed_delayed') )
         {
             debug " *** START : ".$ed_date->desig;
             debug " *** LATEST: ".$c->dsn_date->desig;
             debug " ***  LIMIT: ".($ed_date + $patience)->desig;
 
-            if( $c->dsn_date > $ed_date + $patience )
+            if ( $c->dsn_date > $ed_date + $patience )
             {
 
                 debug "  Not transient anymore";
@@ -742,27 +743,27 @@ sub update_deliverability
             $eda->add({dsn_date => $c->dsn_date}, $args);
         }
     }
-    elsif( $c->is_spam )
+    elsif ( $c->is_spam )
     {
         debug "Is SPAM";
     }
-    elsif( $c->is_ticket )
+    elsif ( $c->is_ticket )
     {
         $ea->add({'has_email_deliverability' =>
                   C->get('ed_queuing')}, $args);
     }
-    elsif( $c->is_vacation )
+    elsif ( $c->is_vacation )
     {
         # Must get more info before we can say anything about this
         # See RDF::Base::Email::Classifier/analyze_vacation
         debug "VACATION. But need more info";
     }
-    elsif( $c->is_delivered )
+    elsif ( $c->is_delivered )
     {
         $ea->update({'has_email_deliverability' =>
                      C->get('ed_deliverable')}, $args);
     }
-    elsif( $c->is_auto_reply )
+    elsif ( $c->is_auto_reply )
     {
         $ea->validate( $args );
     }
@@ -792,7 +793,7 @@ sub validate
 #    my $lea = $ea->first_prop('ea_original');
 #    $lea->validate;
 
-    if( $ea->is_nonhuman )
+    if ( $ea->is_nonhuman )
     {
         $ea->add({has_email_deliverability => C->get('ed_nonhuman')}, $args);
     }
@@ -807,12 +808,12 @@ sub validate
         $test = Para::Frame::Email::Address->parse_tolerant('my@bad.address');
         $test->validate;
     };
-    if( my $err = catch(['email']) )
+    if ( my $err = catch(['email']) )
     {
         debug "ERROR: ".$err->info;
     }
 
-    if( $test )
+    if ( $test )
     {
         debug "Test: ".$test->error_message;
     }
@@ -872,46 +873,46 @@ sub is_nonhuman
     $ea =~ s/^\s*<\s*//; $ea =~ s/\s*>\s*$//;
 
     return $ea =~ qr{
-                     ^bounce-|
-                     \@bounce\.|
-                     -outgoing\@|
-                     -relay\@|
-                     -bounces\@|
-                     -bounce\@|
-                     -errors\@|
-                     ^mailer\@|
-                     ^postmaster|
-                     ^mailer-daemon\@|
-                     ^mailer_daemon\@|
-                     ^majordomo\@|
-                     ^majordom\@|
-                     ^mailman\@|
-                     ^reminder\@|
-                     ^autoreply|
-                     -autoresponder\@|
-                     ^autoresponder\@|
-                     ^server\@|
-                     ^bounce\@|
-                     ^httpd\@|
-                     ^lighttpd\@|
-                     ^nagios\@|
-                     ^fetchmail|
-                     ^listmaster\@|
-                     ^mailmaster\@|
-                     ^squid\@|
-                     ^exim\@|
-                     scomp\@aol.net |
-                     mdaemon\@ |
-                     -request\@ |
-                     ^listserv\@|
-                     ^daemon\@|
-                     ^nobody\@|
-                     ^noreply|
-                     ^no-reply|
-                     ^DoNotReply\@|
-                     ^www-data\@|
-                     ^root\@
-                 }xmi;
+                        ^bounce-|
+                        \@bounce\.|
+                        -outgoing\@|
+                        -relay\@|
+                        -bounces\@|
+                        -bounce\@|
+                        -errors\@|
+                        ^mailer\@|
+                        ^postmaster|
+                        ^mailer-daemon\@|
+                        ^mailer_daemon\@|
+                        ^majordomo\@|
+                        ^majordom\@|
+                        ^mailman\@|
+                        ^reminder\@|
+                        ^autoreply|
+                        -autoresponder\@|
+                        ^autoresponder\@|
+                        ^server\@|
+                        ^bounce\@|
+                        ^httpd\@|
+                        ^lighttpd\@|
+                        ^nagios\@|
+                        ^fetchmail|
+                        ^listmaster\@|
+                        ^mailmaster\@|
+                        ^squid\@|
+                        ^exim\@|
+                        scomp\@aol.net |
+                        mdaemon\@ |
+                        -request\@ |
+                        ^listserv\@|
+                        ^daemon\@|
+                        ^nobody\@|
+                        ^noreply|
+                        ^no-reply|
+                        ^DoNotReply\@|
+                        ^www-data\@|
+                        ^root\@
+                }xmi;
 }
 
 
