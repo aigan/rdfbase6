933dbff67b9c775def7442ebc2a9b6d5259ff931
Jonas Liljegren <jonas@avisita.com>
2014-12-16 10:26:19 +0100

    Indentation

--- lib/RDF/Base/Action/arc_list_delete.pm
+++ lib/RDF/Base/Action/arc_list_delete.pm
@@ -38,30 +38,30 @@ sub handler
     my @arc_id_list;
     foreach my $param ( $q->param )
     {
-	next unless $param =~ /^arc_(\d+)$/;
-	push @arc_id_list, $1;
+        next unless $param =~ /^arc_(\d+)$/;
+        push @arc_id_list, $1;
     }
 
     my $res = RDF::Base::Resource::Change->new;
 
     foreach my $arc_id ( reverse sort @arc_id_list )
     {
-	my $arc = RDF::Base::Resource->get( $arc_id );
-	if( $arc->is_arc )
-	{
-	    $arc->remove( { res => $res } );
-	}
+        my $arc = RDF::Base::Resource->get( $arc_id );
+        if ( $arc->is_arc )
+        {
+            $arc->remove( { res => $res } );
+        }
     }
 
     $res->autocommit;
 
-    if( my $cnt = $res->changes )
+    if ( my $cnt = $res->changes )
     {
-	return "Deleted $cnt arcs";
+        return "Deleted $cnt arcs";
     }
     else
     {
-	return "No change";
+        return "No change";
     }
 }
 
