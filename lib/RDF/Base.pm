f9027d1fa6d11210a2186feac6ae57c3fcf051d2
Jonas Liljegren <jonas@avisita.com>
2023-01-23 12:08:28 +0100

    Details of what arcs changed in restore view

--- lib/RDF/Base.pm
+++ lib/RDF/Base.pm
@@ -1,11 +1,11 @@
-package RDF::Base 6.96;
+package RDF::Base 6.97;
 #=============================================================================
 #
 # AUTHOR
 #		Jonas Liljegren		<jonas@paranormal.se>
 #
 # COPYRIGHT
-#		Copyright (C) 2005-2022 Avisita AB.	 All Rights Reserved.
+#		Copyright (C) 2005-2023 Avisita AB.	 All Rights Reserved.
 #
 #		This module is free software; you can redistribute it and/or
 #		modify it under the same terms as Perl itself.
